e82da8480a3cc454994c7fed849ad767f72aeab6
suraj patil <suraj_patil@live.in>
2026-08-02 21:42:05 +0530

    update clerk domain

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 const config = {
   providers: [
     {
-      domain: "https://clerk.zotion.suraj-patil.in", //"https://complete-basilisk-61.clerk.accounts.dev",
+      domain: "https://clerk.suraj-patil.in", //"https://complete-basilisk-61.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
